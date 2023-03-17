eb3b0adb51fedd7f5ddd3f69a3979d8fe3be55ed
Jaimi Estrada <jaimiestrada007@gmail.com>
2023-03-17 01:26:57 -0400

    Initial commit

---
+++ src/main/java/Distance.java
@@ -0,0 +1,37 @@
+public class Distance {
+
+    public static void main(String[] args) {
+
+        int f = 20;
+        if(f>=0){
+            System.out.println("run 0 - 4 miles, the fee is $2" + ".");
+
+        }
+         if(f>=5){
+            System.out.println("run 5 - 15 miles, the fee is $5" + ".");
+
+        }
+        if(f>=10){
+            System.out.println("run 16 - 25 miles, the fee is $10" + ".");
+
+        }
+         if(f>=15){
+            System.out.println("run 26 - 50 miles, the fee is $15" + ".");
+
+        }
+        if (f>=20){
+            System.out.println("run more than 50 miles, the fee is $20");
+        }
+        }
+
+
+
+
+
+
+
+
+    }
+
+
+
